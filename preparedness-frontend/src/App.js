375df09925ce354f0d7598ae04e846315f69ce5d
Jackson Pruett <jacksonpruett@Jacksons-MBP.lan>
2023-12-09 12:10:11 -0500

    frontend complete

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,34 @@
-import React from "react"
-import { Button, Input, Label } from "reactstrap"
+import React, { useState } from "react"
+import { Button, Input, Label} from 'reactstrap'
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+  // Created a boolean for the modal and initializing it as false
+  const [modal, setModal] = useState(false)
+  // A state with name as a variable and initializes it as empty
+  const [name, setName] = useState("")
+  // handleChange function for the onChange, use e in the parameter for the event
+  // call setName to target the input field and extract the value, setting it to state
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+  // function to toggle the visibility of the modal
+  const toggle = () => setModal(!modal)
+  // function to reset the 'name' state
+  const reset = () => setName("")
+
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+          <Input type="text" onChange={handleChange} value={name}/>
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <Button onClick={toggle}>CLick Me</Button>
+        <Button onClick={reset}>reset</Button>
+        <ModalComponent modal={modal} toggle={toggle} name={name} />
       </div>
     </div>
   )
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,27 @@
-import React from "react"
+import React, { useState } from "react";
+import {Modal, ModalHeader, ModalBody} from 'reactstrap'
 
-const ModalComponent = () => {
-  return <></>
-}
+const ModalComponent = ({modal, name, toggle}) => {
+
+  return (
+    <>
+    <div>
+      {/* using the parameters modal,name,toggle */}
+      <Modal isOpen={modal} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Hello there {name}!</ModalHeader>
+      <ModalBody>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </ModalBody>
+      </Modal>
+      </div>
+    </>
+  );
+};
 
 export default ModalComponent
